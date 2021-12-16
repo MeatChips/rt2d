c8e8dbf0051d467ab2622201eeb3433b2b815442
MeatChips <84376178+MeatChips@users.noreply.github.com>
2021-12-16 09:18:59 +0100

    RT2D Project + Movement

---
+++ SeaWar/myentity.cpp
@@ -0,0 +1,29 @@
+/**
+ * This class describes MyEntity behavior.
+ *
+ * Copyright 2015 Your Name <you@yourhost.com>
+ */
+
+#include "myentity.h"
+
+MyEntity::MyEntity() : Entity()
+{
+	this->rotation.z = PI;
+	this->addSprite("assets/PlayerBoat.tga");
+}
+
+MyEntity::~MyEntity()
+{
+
+}
+
+void MyEntity::update(float deltaTime)
+{
+	// ###############################################################
+	// Rotate
+	// ###############################################################
+	//this->rotation.z += HALF_PI * deltaTime; // 90 deg/sec
+	//if (this->rotation.z > TWO_PI) {
+	//	this->rotation.z -= TWO_PI;
+	//}
+}
---
+++ SeaWar/myscene.cpp
@@ -0,0 +1,95 @@
+/**
+ * This class describes MyScene behavior.
+ *
+ * Copyright 2015 Your Name <you@yourhost.com>
+ */
+
+#include <fstream>
+#include <sstream>
+
+#include "myscene.h"
+
+MyScene::MyScene() : Scene()
+{
+
+	// create a single instance of MyEntity in the middle of the screen.
+	// the Sprite is added in Constructor of MyEntity.
+	myentity = new MyEntity();
+	myentity->position = Point2(SWIDTH/2, SHEIGHT/2);
+	float x;
+	float y;
+	float z;
+
+	// create the scene 'tree'
+	// add myentity to this Scene as a child.
+	this->addChild(myentity);
+}
+
+
+MyScene::~MyScene()
+{
+	// deconstruct and delete the Tree
+	this->removeChild(myentity);
+
+
+
+	// delete myentity from the heap (there was a 'new' in the constructor)
+	delete myentity;
+}
+
+
+void MyScene::update(float deltaTime)
+{
+	// ###############################################################
+	// Escape key stops the Scene
+	// ###############################################################
+	if (input()->getKeyUp(KeyCode::Escape)) {
+		this->stop();
+	}
+
+	// ###############################################################
+	// Movement boat / WASD movement
+	// ###############################################################
+
+	// Variables
+	float a = myentity->rotation.z;
+	float movespeedF = .17;
+	float movespeedB = .08;
+	float rotspeed = 2;
+
+	// Forward
+	if (input()->getKey(KeyCode::W)) {
+		myentity->position.x -= movespeedF * sin(a);
+		myentity->position.y += movespeedF * cos(a);
+												   
+		//std::cout << "FORWARD"; 				   
+	}											   
+		
+	// Backwards
+	if (input()->getKey(KeyCode::S)) {			   
+		myentity->position.x += movespeedB * sin(a);
+		myentity->position.y -= movespeedB * cos(a);
+
+		//std::cout << "BACKWARDS";
+	}
+
+	// Go right
+	if (input()->getKey(KeyCode::D)) {
+		myentity->rotation.z += rotspeed * deltaTime;
+
+		//std::cout << "RIGHT";
+	}
+
+	// Go left
+	if (input()->getKey(KeyCode::A)) {
+		myentity->rotation.z -= rotspeed * deltaTime;
+
+		//std::cout << "LEFT";
+	}
+
+	// Borders
+	if (myentity->position.x < 0) { myentity->position.x = 0; }
+	if (myentity->position.x > SWIDTH) { myentity->position.x = SWIDTH; }
+	if (myentity->position.y < 0) { myentity->position.y = 0; }
+	if (myentity->position.y > SHEIGHT) { myentity->position.y = SHEIGHT; }
+}
